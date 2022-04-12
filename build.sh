7a082d1008bb0f78bd77560bf77119ebd40d8ae6
Guillaume Denis <denisgme@gmail.com>
2022-04-12 13:47:30 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash 
 
 build() {
-  pandoc content/*.md --from markdown+smart --template "./template.tex" --listings -o "document.pdf" --filter pandoc-plantuml --metadata-file metadata.yaml
+  pandoc *.md --from markdown+smart --template "./template.tex" --listings -o "document.pdf" --filter pandoc-plantuml --metadata-file metadata.yaml
 }
 
 build_watch() {
-  inotifywait -r -q -m -e close_write,moved_to,create,delete,delete_self . |
+  inotifywait -q -m -e close_write,moved_to,create,delete,delete_self . |
   while read -r directory events filename; do
-    if [[ ( "$filename" =~ .*md$ && -f "./content/$filename" ) || "$filename" = metadata.yaml ]]; then
-      echo "$filename changed, rebuild document"
+    if [[ "$filename" =~ .*md$ || "$filename" =~ .*\.yaml$ ]]; then
+      echo "$filename $events"
       build
     fi
   done
